da8893f44d7bd582b312fbba06cc0ddaa98cf311
James Healey <jameshealey1994@gmail.com>
2013-10-28 08:41:59 +0000

    Initial commit from NetBeans.
    Basic SimpleTowns class created, package-info added.
    Basic plugin.yml created.

---
+++ SimpleTowns/src/com/gmail/jameshealey1994/simpletowns/SimpleTowns.java
@@ -0,0 +1,17 @@
+package com.gmail.jameshealey1994.simpletowns;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Simple, chunk-based protection plugin for Bukkit.
+ * Further description at http://dev.bukkit.org/bukkit-plugins/simpletowns/
+ * 
+ * @author JamesHealey94 <jameshealey1994.gmail.com>
+ */
+public class SimpleTowns extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // TODO
+    }
+}
---
+++ SimpleTowns/src/com/gmail/jameshealey1994/simpletowns/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The "master"-package, containing everything related to SimpleTowns.
+ */
+package com.gmail.jameshealey1994.simpletowns;
