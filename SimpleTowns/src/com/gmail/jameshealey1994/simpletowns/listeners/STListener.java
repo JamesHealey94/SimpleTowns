91649f0fa5722edccd56ae4804a9cb9db5c86e19
James Healey <jameshealey1994@gmail.com>
2013-12-04 00:57:38 +0000

    Fixed onBlockPlace not being blocked. Added message on chunk move over town borders.

--- SimpleTowns/src/com/gmail/jameshealey1994/simpletowns/listeners/STListener.java
+++ SimpleTowns/src/com/gmail/jameshealey1994/simpletowns/listeners/STListener.java
@@ -2,13 +2,15 @@ package com.gmail.jameshealey1994.simpletowns.listeners;
 
 import com.gmail.jameshealey1994.simpletowns.SimpleTowns;
 import com.gmail.jameshealey1994.simpletowns.object.Town;
-import com.gmail.jameshealey1994.simpletowns.object.TownChunk;
+import java.util.Objects;
 import org.bukkit.Chunk;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 /**
  * Listener class for SimpleTowns plugin.
@@ -36,22 +38,13 @@ public class STListener implements Listener {
      *
      * @param event     event being handled
      */
-    @EventHandler
+    @EventHandler (priority = EventPriority.HIGH)
     public void onBlockBreakEvent(BlockBreakEvent event) {
         final Player player = event.getPlayer();
         final Chunk chunk = event.getBlock().getChunk();
-
-        for (Town t : plugin.getTowns().values()) {
-            for (TownChunk c : t.getTownChunks()) {
-                if (c.equalsChunk(chunk)) {
-                    if (t.hasMember(player.getName())) {
-                        break;
-                    } else {
-                        player.sendMessage("You aren't allowed to build here!"); // TODO - Change
-                        event.setCancelled(true);
-                    }
-                }
-            }
+        if (!canBuild(player, chunk)) {
+            player.sendMessage("You aren't allowed to build here!"); // TODO - Change
+            event.setCancelled(true);
         }
     }
 
@@ -60,22 +53,48 @@ public class STListener implements Listener {
      *
      * @param event     event being handled
      */
-    @EventHandler
+    @EventHandler (priority = EventPriority.HIGH)
     public void onBlockPlaceEvent(BlockPlaceEvent event) {
         final Player player = event.getPlayer();
-        final Chunk chunk = event.getBlockAgainst().getChunk();
+        final Chunk chunk = event.getBlockPlaced().getLocation().getChunk();
+        if (!canBuild(player, chunk)) {
+            player.sendMessage("You aren't allowed to build here!"); // TODO - Change
+            event.setCancelled(true);
+        }
+    }
 
-        for (Town t : plugin.getTowns().values()) {
-            for (TownChunk c : t.getTownChunks()) {
-                if (chunk.equals(c)) {
-                    if (t.getCitizens().contains(player.getName()) || t.getLeaders().contains(player.getName())) {
-                        break;
-                    } else {
-                        player.sendMessage("You aren't allowed to build here!"); // TODO - Change
-                        event.setCancelled(true);
-                    }
-                }
+    /**
+     * Sends player a message when moving in / out of Town chunks.
+     *
+     * @param event     event being handled
+     */
+    @EventHandler (priority = EventPriority.NORMAL)
+    public void onPlayerMove(PlayerMoveEvent event) {
+        final Town exited = plugin.getTown(event.getFrom().getChunk());
+        final Town entered = plugin.getTown(event.getTo().getChunk());
+        if (!Objects.equals(exited, entered)) {
+            if (exited != null) {
+                event.getPlayer().sendMessage("exited " + exited.getName());
+            }
+            if (entered != null) {
+                event.getPlayer().sendMessage("entered " + entered.getName());
             }
         }
+    }
+
+    /**
+     * Returns if a player can build in a chunk.
+     * (if the chunk belongs to a town they are a member of)
+     *
+     * @param player        player being checked
+     * @param chunk         chunk being checked
+     * @return              if the player can build
+     */
+    private boolean canBuild(Player player, Chunk chunk) {
+        final Town town = plugin.getTown(chunk);
+        if (town != null && !town.hasMember(player.getName())) {
+            return false;
+        }
+        return true;
     }
 }
