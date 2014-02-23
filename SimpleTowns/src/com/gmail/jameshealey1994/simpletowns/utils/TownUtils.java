29de241f8c616af4c70d540e6a6452fd185a82a5
JamesHealey94 <jameshealey1994@gmail.com>
2014-02-23 02:05:19 +0000

    Extracted getConfigSection from getTownsFromConfig.
    Improved reusability of getConfigSection.

--- SimpleTowns/src/com/gmail/jameshealey1994/simpletowns/utils/TownUtils.java
+++ SimpleTowns/src/com/gmail/jameshealey1994/simpletowns/utils/TownUtils.java
@@ -25,21 +25,14 @@ public final class TownUtils {
     /**
      * Returns towns from config.
      *
-     * @param plugin        plugin with config
+     * @param plugin        plugin with config and logger
      * @return              towns from config
      */
     public static Map<String, Town> getTownsFromConfig(Plugin plugin) {
+        final ConfigurationSection townConfigSection = getConfigSection(plugin, PATH);
         final Map<String, Town> townsFromConfig = new HashMap<>();
-
-        ConfigurationSection townConfigSection = plugin.getConfig().getConfigurationSection(PATH);
-        if (townConfigSection == null) {
-            plugin.getLogger().log(Level.INFO, "Config section ''{0}'' not found. Creating...", PATH);
-            townConfigSection = plugin.getConfig().createSection(PATH);
-            plugin.saveConfig();
-            plugin.getLogger().log(Level.INFO, "Config section ''{0}'' created", PATH);
-        }
-
         final Set<String> townKeys = new HashSet<>(townConfigSection.getKeys(false));
+
         for (String townname : townKeys) {
             try {
                 final Set<String> leaders = new HashSet<>(plugin.getConfig().getStringList(PATH + "." + townname + ".Leaders"));
@@ -62,6 +55,45 @@ public final class TownUtils {
             }
         }
         return townsFromConfig;
+    }
+
+    /**
+     * Returns a configuration section from a passed plugin and path.
+     * If no configuration section is found, an empty configuration section
+     * is created, saved, and returned.
+     *
+     * @param plugin    plugin with a configuration
+     * @param path      path to the configuration section to return
+     * @return          configuration section in config of passed plugin
+     *                  with passed path
+     */
+    private static ConfigurationSection getConfigSection(Plugin plugin, String path) {
+        ConfigurationSection configSection = plugin.getConfig().getConfigurationSection(path);
+        if (configSection == null) {
+            configSection = createEmptyConfigSection(plugin, path);
+        }
+        return configSection;
+    }
+
+    /**
+     * Creates an empty ConfigurationSection
+     * at the passed path in the config of the passed plugin.
+     * The config is then saved, and the empty ConfigurationSection returned.
+     *
+     * Any value that was previously set at this path will be overwritten.
+     * If the previous value was itself a ConfigurationSection,
+     * it will be orphaned.
+     *
+     * @param plugin    plugin with a configuration
+     * @param path      path to the configuration section to create or overwrite
+     * @return          an empty configuration section
+     */
+    private static ConfigurationSection createEmptyConfigSection(Plugin plugin, String path) {
+        plugin.getLogger().log(Level.INFO, "Config section ''{0}'' not found. Creating...", path);
+        final ConfigurationSection emptyConfigSection = plugin.getConfig().createSection(path);
+        plugin.saveConfig();
+        plugin.getLogger().log(Level.INFO, "Config section ''{0}'' created", path);
+        return emptyConfigSection;
     }
 
     /**
